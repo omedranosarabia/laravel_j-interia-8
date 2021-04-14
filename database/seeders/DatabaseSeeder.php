93a91db76e89c0b11f71911266624ea8ae8611f0
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-14 12:03:49 -0500

    Configuración Inicial

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'email' => 'o@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Note::factory(30)->create();
+
+    }
+}
