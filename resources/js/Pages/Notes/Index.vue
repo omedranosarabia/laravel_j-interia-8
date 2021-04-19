a36bad388155e640f621fa44c7c6c47267d6635c
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-19 17:47:03 -0500

    Listado de notas: personalizacion

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -7,14 +7,35 @@
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="md:grid md-grid-cols-3 md:gap">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">  
+                <div class="md:grid md:grid-cols-3 md:gap-6">
                     <div class="md:col-span-1">
                         <div class="px-4 sm:px0">
                             <h3 class="text-lg text-gray-900">Listado de notas</h3>
                             <p class="text-sm text-gray-600">
                                 Toma el registro correcto y ejecuta cualquier función (ver, editar o eliminar)
                             </p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <table>
+                                <tr v-for="note in notes">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.show', note.id)">
+                                            Ver
+                                        </inertia-link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.edit', note.id)">
+                                            Editar
+                                        </inertia-link>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -31,5 +52,8 @@
         components: {
             AppLayout,
         },
+        props:{
+            notes: Array,
+        }
     }
 </script>
